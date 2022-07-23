355cc70897118c771a22198c3aa2deedb2852e8e
KESAVAREDDYNEW <103446052+KESAVAREDDYNEW@users.noreply.github.com>
2022-07-23 10:50:20 +0400

    Add files via upload

---
+++ employee-rest/src/main/java/com/glearning/employeesapi/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.glearning.employeesapi.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gl.employeesapi.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
+
